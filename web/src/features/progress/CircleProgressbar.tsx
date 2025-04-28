046889f8a003b4fa5a37b6f631992cc3d15ccc7c
Emilyuwu0 <tbarrera679@gmail.com>
2025-04-28 17:53:52 -0500

    Design interface x10

--- web/src/features/progress/CircleProgressbar.tsx
+++ web/src/features/progress/CircleProgressbar.tsx
@@ -1,62 +1,16 @@
 import React from 'react';
-import {createStyles, keyframes, RingProgress, Stack, Text, useMantineTheme} from '@mantine/core';
-import {useNuiEvent} from '../../hooks/useNuiEvent';
-import {fetchNui} from '../../utils/fetchNui';
+import { Stack } from '@mantine/core';
+import { useNuiEvent } from '../../hooks/useNuiEvent';
+import { fetchNui } from '../../utils/fetchNui';
 import ScaleFade from '../../transitions/ScaleFade';
-import type {CircleProgressbarProps} from '../../typings';
-
-// 33.5 is the r of the circle
-const progressCircle = keyframes({
-  '0%': { strokeDasharray: `0, ${33.5 * 2 * Math.PI}` },
-  '100%': { strokeDasharray: `${33.5 * 2 * Math.PI}, 0` },
-});
-
-const useStyles = createStyles((theme, params: { position: 'middle' | 'bottom'; duration: number }) => ({
-  container: {
-    width: '100%',
-    height: params.position === 'middle' ? '100%' : '20%',
-    bottom: 0,
-    position: 'absolute',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  progress: {
-    '> svg > circle:nth-child(1)': {
-      stroke: theme.colors.dark[5],
-    },
-    // Scuffed way of grabbing the first section and animating it
-    '> svg > circle:nth-child(2)': {
-      transition: 'none',
-      animation: `${progressCircle} linear forwards`,
-      animationDuration: `${params.duration}ms`,
-    },
-  },
-  value: {
-    textAlign: 'center',
-    fontFamily: 'Roboto Mono',
-    textShadow: theme.shadows.sm,
-    color: theme.colors.gray[3],
-  },
-  label: {
-    textAlign: 'center',
-    textShadow: theme.shadows.sm,
-    color: theme.colors.gray[3],
-    height: 25,
-  },
-  wrapper: {
-    marginTop: params.position === 'middle' ? 25 : undefined,
-  },
-}));
+import type { CircleProgressbarProps } from '../../typings';
+import './index.css';
 
 const CircleProgressbar: React.FC = () => {
   const [visible, setVisible] = React.useState(false);
   const [progressDuration, setProgressDuration] = React.useState(0);
   const [position, setPosition] = React.useState<'middle' | 'bottom'>('middle');
   const [value, setValue] = React.useState(0);
-  const [label, setLabel] = React.useState('');
-  const theme = useMantineTheme();
-  const { classes } = useStyles({ position, duration: progressDuration });
 
   useNuiEvent('progressCancel', () => {
     setValue(99);
@@ -67,37 +21,64 @@ const CircleProgressbar: React.FC = () => {
     if (visible) return;
     setVisible(true);
     setValue(0);
-    setLabel(data.label || '');
     setProgressDuration(data.duration);
     setPosition(data.position || 'middle');
     const onePercent = data.duration * 0.01;
     const updateProgress = setInterval(() => {
-      setValue((previousValue) => {
-        const newValue = previousValue + 1;
-        newValue >= 100 && clearInterval(updateProgress);
-        return newValue;
+      setValue((prev) => {
+        const newVal = prev + 1;
+        if (newVal >= 100) {
+          clearInterval(updateProgress);
+          setVisible(false); // 👈 aquí ocultas cuando termina
+        }
+        return newVal;
       });
     }, onePercent);
   });
 
   return (
-    <>
-      <Stack spacing={0} className={classes.container}>
-        <ScaleFade visible={visible} onExitComplete={() => fetchNui('progressComplete')}>
-          <Stack spacing={0} align="center" className={classes.wrapper}>
-            <RingProgress
-              size={90}
-              thickness={7}
-              sections={[{ value: 0, color: theme.primaryColor }]}
-              onAnimationEnd={() => setVisible(false)}
-              className={classes.progress}
-              label={<Text className={classes.value}>{value}%</Text>}
-            />
-            {label && <Text className={classes.label}>{label}</Text>}
-          </Stack>
-        </ScaleFade>
-      </Stack>
-    </>
+    <Stack
+      spacing={0}
+      style={{
+        width: '100%',
+        height: position === 'middle' ? '100%' : '20%',
+        bottom: 0,
+        position: 'absolute',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <ScaleFade visible={visible} onExitComplete={() => fetchNui('progressComplete')}>
+        <div className="w-[4rem] h-[4rem] relative">
+          <div className="spinner">
+            <div className="spinner1"></div>
+          </div>
+
+          <div
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              color: 'white',
+              fontWeight: 'bold',
+            }}
+          >
+            % {value}
+          </div>
+
+          <div
+            style={{
+              position: 'absolute',
+              top: '60%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+            }}
+          ></div>
+        </div>
+      </ScaleFade>
+    </Stack>
   );
 };
 
